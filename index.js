2d713843f997a0afb6a02c7d4b07df58ac93c9b1
Jennifer Man <jenniferman702@gmail.com>
2022-09-21 13:40:06 -0400

    commit tree d1808fc4ed9cd73034833f430f615591972f7581

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0,2);
+  }
+
+const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(-2);
+}
